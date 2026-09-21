8843bffbf0ee50348e94ce932bfcf682ea374bc7
JulesBotty <julesbotty33@gmail.com>
2026-09-21 10:06:53 +0000

    actu 21/09/2026 2

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,8 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 //const PORT = process.env.PORTS || 3000;
-const allowedOrigins = (process.env.CORS_ORIGIN || "")
-//const allowedOrigins = (process.env.CORS_API_ORIGIN || "")
+//const allowedOrigins = (process.env.CORS_ORIGIN || "")
+const allowedOrigins = (process.env.CORS_API_ORIGIN || "")
   .split(",")
   .map((origin) => origin.trim())
   .filter(Boolean);
